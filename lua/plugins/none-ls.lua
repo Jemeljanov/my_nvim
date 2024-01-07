0efb086912c389e6a1ddea1e94ed0732a8e6a69a
Jemeljanov <jemeljanov.dmitri@gmail.com>
2024-01-07 13:24:19 +0100

    add python

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,6 +6,10 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        -- null_ls.builtins.diagnostics. to_add,
+        -- python
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
       },
     })
 
