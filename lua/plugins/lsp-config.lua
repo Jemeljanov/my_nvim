1f7b01dbce6db42202529b9246c16ef17ae805bf
Jemeljanov <jemeljanov.dmitri@gmail.com>
2025-01-12 01:12:44 +0100

    replace deprecated tsserver with ts_ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "pyright" },
+				ensure_installed = { "lua_ls", "ts_ls", "pyright" }, 
 			})
 		end,
 	},
@@ -20,7 +20,7 @@ return {
 
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.ts_ls.setup({ capabilities = capabilities }) 
 			lspconfig.pyright.setup({ capabilities = capabilities })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
